209be703f52dec5edd714063eac994155d7359c3
Arputha Patric Jesu Durai <mano.snow@gmail.com>
2017-10-08 20:28:34 -0700

    week 1 - home work

---
+++ src/AboutmeHW1.java
@@ -0,0 +1,18 @@
+
+public class AboutmeHW1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I'm Manoja.");
+		System.out.println("I am from india");
+		System.out.print("I have two children.");
+		System.out.println("I love my children verymuch.");
+		System.out.println("I came to US 7yrs ago.");
+		System.out.print("I wanted to enter into the software industry.");
+		System.out.print("So I took QA101 Software testing course.");
+		System.out.println("I learnt Testing tools and techniques there.");
+		System.out.print("I knew multiple languages like JAVA,.net ,SQL etc.");
+		System.out.println("I am doing Selenium course now.");
+	}
+
+}
